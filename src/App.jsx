3e4fe9e0ac96df320a4e96337f38c190ae6fedf7
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-18 20:58:55 +1000

    second

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelListContainer, ChannelContainer } from "./components";
+
+const apiKey = "nmmxf3j5qbza";
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
