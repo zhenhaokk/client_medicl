4754a8da156330713a171e65be4f3852206fb78c
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-19 23:41:11 +1000

    fouth

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,11 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
+
+const cookies = new Cookies();
+
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={HospitalIcon} alt="Hospital" width="30" />
+      </div>
+    </div>
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="Logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Medical Pager</p>
+  </div>
+);
+
 const ChannelListContainer = () => {
   return (
-    <div>ChannelListContainer</div>
-  )
-}
+    <>
+      <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type="team"
+              // isCreating={isCreating}
+              // setIsCreating={setIsCreating}
+              // setCreateType={setCreateType}
+              // setIsEditing={setIsEditing}
+              // setToggleContainer={setToggleContainer}
+            />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              // setIsCreating={setIsCreating}
+              // setIsEditing={setIsEditing}
+              // setToggleContainer={setToggleContainer}
+              type="team"
+            />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type="messaging"
+              // isCreating={isCreating}
+              // setIsCreating={setIsCreating}
+              // setCreateType={setCreateType}
+              // setIsEditing={setIsEditing}
+              // setToggleContainer={setToggleContainer}
+            />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              // setIsCreating={setIsCreating}
+              // setIsEditing={setIsEditing}
+              // setToggleContainer={setToggleContainer}
+              type="messaging"
+            />
+          )}
+        />
+      </div>
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;
