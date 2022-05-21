8cd06413c64005dcdd97f81f131d29c4cd1e0e60
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-21 21:15:47 +1000

    final

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
     const { username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "http://localhost:5000/auth";
+    const URL = "https://medical-chart-js.herokuapp.com/auth";
     // const URL = 'https://medical-pager.herokuapp.com/auth';
 
     const {
