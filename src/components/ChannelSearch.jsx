29602d8332fa9030d75665ef92a5dcd26bad2277
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-05-21 20:23:51 +1000

    eig

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,29 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { useChatContext } from "stream-chat-react";
 
+import { ResultsDropdown } from "./";
 import { SearchIcon } from "../assets";
 
-const ChannelSearch = () => {
+const ChannelSearch = ({ setToggleContainer }) => {
   const { client, setActiveChannel } = useChatContext();
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
   const [teamChannels, setTeamChannels] = useState([]);
   const [directChannels, setDirectChannels] = useState([]);
 
-  const getChannels = async (text) => { 
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([]);
+      setDirectChannels([]);
+    }
+  }, [query]);
+
+  const getChannels = async (text) => {
     try {
-      
+      const channelResponse = client.queryChannels({
+        type: "team",
+        name: { $autocomplete: text },
+        members: { $in: [client.userID] },
+      });
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },
+        name: { $autocomplete: text },
+      });
+
+      const [channels, { users }] = await Promise.all([
+        channelResponse,
+        userResponse,
+      ]);
+
+      if (channels.length) setTeamChannels(channels);
+      if (users.length) setDirectChannels(users);
     } catch (error) {
       setQuery("");
     }
-  }
+  };
 
   const onSearch = (event) => {
     event.preventDefault();
 
     setLoading(true);
     setQuery(event.target.value);
-    // getChannels(event.target.value);
+    getChannels(event.target.value);
+  };
+
+  const setChannel = (channel) => {
+    setQuery("");
+    setActiveChannel(channel);
   };
 
   return (
@@ -40,6 +69,16 @@ const ChannelSearch = () => {
           onChange={onSearch}
         />
       </div>
+      {query && (
+        <ResultsDropdown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+          setToggleContainer={setToggleContainer}
+        />
+      )}
     </div>
   );
 };
